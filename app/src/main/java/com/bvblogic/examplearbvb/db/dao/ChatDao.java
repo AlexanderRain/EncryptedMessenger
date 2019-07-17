d03a1ebbd3f09d67e95de3e9f43e4593f7b8d65c
AlexanderRain <alexanderzzip@gmail.com>
2019-07-17 20:41:36 +0300

    database implementation

---
+++ app/src/main/java/com/bvblogic/examplearbvb/db/dao/ChatDao.java
@@ -0,0 +1,22 @@
+package com.bvblogic.examplearbvb.db.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.bvblogic.examplearbvb.db.domain.Chat;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+@Dao
+public interface ChatDao {
+
+    @Query("SELECT * FROM chat")
+    Single<List<Chat>> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    List<Long> insertAll(Chat... chats);
+}
---
+++ app/src/main/java/com/bvblogic/examplearbvb/db/dao/MessageDao.java
@@ -0,0 +1,7 @@
+package com.bvblogic.examplearbvb.db.dao;
+
+import android.arch.persistence.room.Dao;
+
+@Dao
+public interface MessageDao {
+}
