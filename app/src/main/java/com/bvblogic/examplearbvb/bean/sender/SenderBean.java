25d47073e9cb0024f76c96ec400b41c8d633f51d
Alex <40667270+AlexKutny@users.noreply.github.com>
2019-07-22 03:29:58 +0300

    creating_json_file

--- app/src/main/java/com/bvblogic/examplearbvb/bean/sender/SenderBean.java
+++ app/src/main/java/com/bvblogic/examplearbvb/bean/sender/SenderBean.java
@@ -2,11 +2,16 @@ package com.bvblogic.examplearbvb.bean.sender;
 
 import android.content.Intent;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.bvblogic.examplearbvb.bean.core.Bean;
 import com.bvblogic.examplearbvb.db.domain.Chat;
 import com.bvblogic.examplearbvb.db.domain.SendAction;
+import com.bvblogic.examplearbvb.utils.Constants;
+import com.bvblogic.examplearbvb.utils.SecondaryKeyTask;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.androidannotations.annotations.EBean;
 
@@ -15,13 +20,47 @@ public class SenderBean extends Bean {
 
     private SendAction sendAction;
     private String address;
+    private String filePass;
+    private int primaryKeyForWhile;
 
     public void init(Chat chat) {
         this.sendAction = chat.getType();
         this.address = chat.getAddress();
+        // we have not any data in such fields
+        this.filePass = "somedata";
+        //  due to we still have not primary key, i have used this
+        this.primaryKeyForWhile = chat.getId();
+
+    }
+
+    private void beforeSending(){
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        int key = 0;
+
+        for(int i = 0; i < filePass.length(); i++){
+
+            if (i == 0) key = filePass.charAt(i);
+            else key *= filePass.charAt(i);
+        }
+
+        key *= primaryKeyForWhile;
+        key %= Constants.PASSWORD_DIVIDER;
+
+        String json = gson.toJson(key); // String json = gson.toJson(new String[]{key, others});
+
+
+        SecondaryKeyTask keyTask = new SecondaryKeyTask(activity);
+        keyTask.execute(json);
+
+//      Log.d("JSON", json);
     }
 
     public void send(String message) {
+
+        beforeSending();
+
         switch(sendAction){
             case SMS:
                 try {
--- app/src/main/java/com/bvblogic/examplearbvb/utils/Constants.java
+++ app/src/main/java/com/bvblogic/examplearbvb/utils/Constants.java
@@ -3,5 +3,7 @@ package com.bvblogic.examplearbvb.utils;
 public class Constants {
     public static final String SMS_ACTION = "SMS";
     public static final String EMAIL_ACTION = "EMAIL";
+    public static final String KEYS_FILE_NAME = "personal/chatId+keys.json";
     public static final int PERMISSION_REQUEST_SMS = 0;
+    public static final int PASSWORD_DIVIDER = (int)Math.pow(2, 34);
 }
