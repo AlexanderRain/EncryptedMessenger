57b4a7c61bc451daa7f6bfa8fb5eea65fb0999d5
Artem <greatwolf20xx@gmail.com>
2019-07-24 19:22:44 +0300

    checkPassword

--- app/src/main/java/com/bvblogic/examplearbvb/fragment/NewMessageFragment.java
+++ app/src/main/java/com/bvblogic/examplearbvb/fragment/NewMessageFragment.java
@@ -2,28 +2,25 @@ package com.bvblogic.examplearbvb.fragment;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.bvblogic.examplearbvb.R;
-import com.bvblogic.examplearbvb.activity.core.BaseActivity;
-import com.bvblogic.examplearbvb.bean.sender.SenderBean;
-import com.bvblogic.examplearbvb.db.domain.SendAction;
 import com.bvblogic.examplearbvb.db.presenter.UserChatPresenter;
 import com.bvblogic.examplearbvb.fragment.core.BaseFragment;
 import com.bvblogic.examplearbvb.mvp.core.FragmentById;
 import com.bvblogic.examplearbvb.mvp.core.FragmentData;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
-import org.androidannotations.annotations.RootContext;
 import org.androidannotations.annotations.ViewById;
 
 import static com.bvblogic.examplearbvb.utils.Constants.PERMISSION_REQUEST_SMS;
@@ -50,14 +47,40 @@ public class NewMessageFragment extends BaseFragment {
 
     @ViewById(R.id.btnEnter)
     Button btnEnter;
+
+    @ViewById(R.id.btnSend)
+    Button btnSend;
+
+    @ViewById(R.id.fragment_container)
+    RelativeLayout layout;
+
+    @ViewById(R.id.enter_filePasword)
+    MaterialEditText enter_pass;
+
     @Click(R.id.btnEnter)
     public void enter(){
-        btnEnter.setVisibility(View.INVISIBLE);
+        if(!enter_pass.getText().toString().equals("")){
+            btnEnter.setVisibility(View.INVISIBLE);
+            enter_pass.setVisibility(View.INVISIBLE);
+            btnSend.setEnabled(true);
+        }
     }
 
 
     @AfterViews
     public void init(){
+        btnEnter.setVisibility(View.INVISIBLE);
+        enter_pass.setVisibility(View.INVISIBLE);
+        if (enter_pass.getText().toString().equals(""))
+        {
+            Toast toast = Toast.makeText(getContext(),"Пароль не введён.", Toast.LENGTH_SHORT);
+            toast.show();
+            enter_pass.setError("Это поле не может быть пустым.");
+            btnEnter.setVisibility(View.VISIBLE);
+            enter_pass.setVisibility(View.VISIBLE);
+        }else{
+            btnSend.setEnabled(true);
+        }
         userPresenter.getUser(chatId);
         // request permissions
         requestPermission();
